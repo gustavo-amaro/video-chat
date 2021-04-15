008751a742c2865d7e890fd28df94d632abcb4f8
Gustavo <gustavo.maro97@gmail.com>
2021-04-15 14:09:38 -0300

    fix peer server

--- public/script.js
+++ public/script.js
@@ -1,10 +1,11 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-  host: "/",
-  port: "9001",
+  host: "peerserver.quizclass.com.br",
+  secure: true,
 });
 
+console.log(myPeer);
 const myVideo = document.createElement("video");
 
 myVideo.muted = true;
